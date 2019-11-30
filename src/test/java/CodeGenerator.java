4a5105fad44a21bec1e75c42f3519525cbe05635
good0520 <52948529+good0520@users.noreply.github.com>
2019-11-30 11:23:27 +0800

    Update CodeGenerator.java
    代码生成连接字符串添加nullCatalogMeansCurrent=true

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -18,7 +18,7 @@ import static com.company.project.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/cpmanager"+"?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/cpmanager"+"?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8&nullCatalogMeansCurrent=true";
     private static final String JDBC_USERNAME = "root";
     private static final String JDBC_PASSWORD = "123456";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
