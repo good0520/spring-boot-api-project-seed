d93c16ea269e13df272cdf790c287dea16c9bed8
good0520 <52948529+good0520@users.noreply.github.com>
2020-03-16 11:20:01 +0800

    add swagger config

---
+++ src/main/java/com/company/project/configurer/SwaggerConfig.java
@@ -0,0 +1,77 @@
+package com.holdingfuture.exhibition.configurer;
+
+import com.spring4all.swagger.SwaggerProperties;
+import io.swagger.annotations.Api;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zcx
+ * @date 2020/3/9 11:26
+ */
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    @ConditionalOnMissingBean
+    public SwaggerProperties swaggerProperties() {
+        return new SwaggerProperties();
+    }
+
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class)).paths(PathSelectors.any()).build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+    private List<ApiKey> securitySchemes() {
+        List<ApiKey> apiKeyList= new ArrayList<ApiKey>();
+        apiKeyList.add(new ApiKey("Authorization", "Authorization", "header"));
+        return apiKeyList;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContexts=new ArrayList<>();
+        securityContexts.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build());
+        return securityContexts;
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences=new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+        return securityReferences;
+    }
+
+    // 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 页面标题1
+                .title(swaggerProperties().getTitle())
+                // 描述
+                .description(swaggerProperties().getDescription()).termsOfServiceUrl(swaggerProperties().getTermsOfServiceUrl())
+                // 创建人
+                .contact(new Contact(swaggerProperties().getContact().getName(), swaggerProperties().getContact().getUrl(), swaggerProperties().getContact().getUrl()))
+                // 版本号
+                .version(swaggerProperties().getVersion()).build();
+    }
+
+}
